9a5a77024eedee860fd29256e1e968fa084186dd
YaningX <yaning0123@gmail.com>
2015-12-13 11:06:33 +0800

    implement configuration module

--- common/configure/src/test/java/com/onion/conf/ConfigLoaderTest.java
+++ common/configure/src/test/java/com/onion/conf/ConfigLoaderTest.java
@@ -1,3 +1,13 @@
+package com.onion.conf;
+
+
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  *  Licensed to the Apache Software Foundation (ASF) under one
  *  or more contributor license agreements.  See the NOTICE file
@@ -6,20 +16,32 @@
  *  to you under the Apache License, Version 2.0 (the
  *  "License"); you may not use this file except in compliance
  *  with the License.  You may obtain a copy of the License at
- *  
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing,
  *  software distributed under the License is distributed on an
  *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  *  KIND, either express or implied.  See the License for the
  *  specific language governing permissions and limitations
- *  under the License. 
- *  
+ *  under the License.
+ *
  */
-package com.onion.conf;
+public class ConfigLoaderTest {
+    @Test
+    public void loadTest() throws IOException {
+        Properties properties = new Properties();
+        InputStream input = new FileInputStream(System.getProperty("user.dir") + "/conf.properties");
+        properties.load(input);
+        System.out.println(properties.getProperty("name"));
+    }
 
-public interface ConfigKey {
-    String getPropertyKey();
-    Object getDefaultValue();
+    @Test
+    public void test() {
+        Conf conf = new Conf();
+        conf.loadConf(System.getProperty("user.dir") + "/conf.properties");
+        System.out.print(conf.getKeys());
+    }
+
+
 }

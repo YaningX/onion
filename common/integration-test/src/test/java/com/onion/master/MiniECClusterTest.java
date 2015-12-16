25c8fb884c451242ddc4540f7d26d34d9f5835ec
sunhonglin <honggexiao@sina.com>
2015-12-16 22:25:50 +0800

    update MiniECClusterTest

--- common/integration-test/src/test/java/com/onion/master/MiniECClusterTest.java
+++ common/integration-test/src/test/java/com/onion/master/MiniECClusterTest.java
@@ -14,9 +14,47 @@
  */
 package com.onion.master;
 
+import com.onion.eclib.CauchyGoodRSCoder;
+import com.onion.eclib.ECHandler;
+import com.onion.eclib.ErasureCoder;
+import com.onion.worker.Worker;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+
 /**
  * A cluster based on erasure code.
  */
 public class MiniECClusterTest {
+    private static Worker[] workers = new Worker[9];
 
+    public void doWrite(byte[][] encodeData, Worker[] workers) throws IOException {
+        int count = 0;
+        MasterBlockWriter writer = new MasterBlockWriter();
+        for (int i = 0; i < encodeData.length; i++) {
+            int nextWorker = count % workers.length;
+            writer.open(workers[nextWorker].getWorkerAddress(), i, 0);
+            writer.write(encodeData[i], 0, encodeData[i].length);
+            writer.close();
+            count++;
+        }
+    }
+
+    @BeforeClass
+    public static void step() {
+        for (int i = 0; i < workers.length; i++) {
+            workers[i] = new Worker(System.getProperty("user.dir") +
+                    "/src/test/conf/worker" + (i+1) + ".conf");
+            workers[i].process();
+        }
+    }
+
+    @Test
+    public void remoteEncodeBlockWriteTest() throws IOException{
+        ErasureCoder coder = new CauchyGoodRSCoder(6, 3, 8, 8);
+        ECHandler handler = new ECHandler(6, 3, coder, 8, 8);
+        byte[][] encodeData = handler.encode(System.getProperty("user.dir") + "/pom.xml");
+        doWrite(encodeData, workers);
+    }
 }
